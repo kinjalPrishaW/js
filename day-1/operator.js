f678b8bbaf63b418b4a16119c60a8878458c4eaa
kinjalPrishaW <133582299+kinjalPrishaW@users.noreply.github.com>
2024-07-04 17:10:20 +0530

    add files

---
+++ day-1/operator.js
@@ -0,0 +1,25 @@
+//adding two numbers
+var x=10;
+var y=20;
+z=x+y;
+console.log(z)
+// adding the number using let keyword
+let a=18;
+let b=34;
+let c=a+b;
+console.log(c)
+// assigning value into variable
+var x=5;
+console.log(x)
+
+//multiply two numbers
+let l=7;
+let m=10;
+let n=l*m;
+console.log(n) 
+
+
+let text1 = "What a very ";
+text1 += "nice day";
+console.log(text1)
+
