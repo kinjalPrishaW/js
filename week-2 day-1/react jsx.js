1726bbe16857b4f4914814e157ac777cb1bdd380
kinjalPrishaW <133582299+kinjalPrishaW@users.noreply.github.com>
2024-07-09 09:50:55 +0530

    add files

---
+++ week-2 day-1/react jsx.js
@@ -0,0 +1,24 @@
+import React from "react";
+const Header=()=>{
+    return(
+        <div>helloo alll</div>
+    )
+}
+export default Header;
+
+import React from "react";
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+  
+  const element = <Welcome name="Sara" />;
+  export  default Welcome;
+
+import React from "react";
+const Greetings=()=>{
+    return(
+        <div>helloo alll</div>
+    )
+}
+export default Greetings;
+
