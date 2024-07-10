8dc8bef7179096d85049abf4f2a8706629bf442d
kinjalPrishaW <133582299+kinjalPrishaW@users.noreply.github.com>
2024-07-10 13:01:23 +0530

    add files

---
+++ week-2 day-2/classs.js
@@ -0,0 +1,8 @@
+import React,{ Component} from 'react'
+class Welcomes extends Component {
+         render(){
+            return <h1>Class Component</h1>
+         }
+
+         }
+export default  Welcomes;// class component
