b099ab025701372fc29ec7d88e91b729342dbf10
kinjalPrishaW <133582299+kinjalPrishaW@users.noreply.github.com>
2024-07-11 10:04:43 +0530

    add

---
+++ week-2 day-3/app.js
@@ -0,0 +1,17 @@
+import Application from "./components/onclick";
+import OnSubmit from "./components/Onsubmit";
+import Ifelse from './components/if else';
+function App() {
+
+  return (
+    <div > 
+     <Greeting/>
+    <Application/> 
+      <OnSubmit/>
+      <Ifelse />
+      
+    </div> 
+  );
+}
+
+export default App;
